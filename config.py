ccb6317e20087d4585c0f9abb0aff330504d7088
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-05-20 06:50:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "10247139"
+
+API_HASH = "96b46175824223a33737657ab943fd6a"
+
+BOT_TOKEN = "5222572158:AAENHtTOnhWBh4UUZKTjq5ruMtil_4zRA_0"
